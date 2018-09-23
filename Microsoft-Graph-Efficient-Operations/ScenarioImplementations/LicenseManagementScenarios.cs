50377a87e2f1aa2fcde543ca5febf55e8adaa654
Peter Ciszewski <piotrci@microsoft.com>
2018-09-23 15:26:49 -0700

    Added comments to license scenarios.

--- Microsoft-Graph-Efficient-Operations/ScenarioImplementations/LicenseManagementScenarios.cs
+++ Microsoft-Graph-Efficient-Operations/ScenarioImplementations/LicenseManagementScenarios.cs
@@ -11,10 +11,20 @@ namespace ScenarioImplementations
 {
     public static class LicenseManagementScenarios
     {
+        /// <summary>
+        /// Adds a specific license to selected users.
+        /// </summary>
+        /// <param name="requestManager"></param>
+        /// <param name="users">A collection of users for whom to add the license. Note that we only really need the Id value of each user, so this method could be refactored.</param>
+        /// <param name="skuPartNumber">The name of the product, e.g. "ENTERPRISEPACK" for Office365 E3. We resolve this later to a SKU id (guid)</param>
+        /// <returns></returns>
         public static IEnumerable<OperationResult<User>> AssignLicensesToUsers(RequestManager requestManager, IEnumerable<User> users, string skuPartNumber)
         {
+            // Resolve the product name to the guid id
             var licenseId = GetLicenseId(requestManager.GraphClient, skuPartNumber);
+            // Build the input parameter
             var licenseToAssign = new AssignedLicense { SkuId = licenseId, DisabledPlans = new Guid[0] };
+            // Execute interesting code
             return ModifyLicensesForUsers(requestManager, users, new AssignedLicense[] { licenseToAssign }, new Guid[0]);
         }
         public static IEnumerable<OperationResult<User>> RemoveLicensesFromUsers(RequestManager requestManager, IEnumerable<User> users, string skuPartNumber)
@@ -22,15 +32,29 @@ namespace ScenarioImplementations
             var licenseId = GetLicenseId(requestManager.GraphClient, skuPartNumber);
             return ModifyLicensesForUsers(requestManager, users, new AssignedLicense[0], new Guid[] { licenseId });
         }
+        /// <summary>
+        /// Executes license modification requests on users. Uses parallelization and batching of requests to maximize throughput.
+        /// </summary>
+        /// <param name="requestManager"></param>
+        /// <param name="users"></param>
+        /// <param name="licensesToAdd"></param>
+        /// <param name="licensesToRemove"></param>
+        /// <returns></returns>
         private static IEnumerable<OperationResult<User>> ModifyLicensesForUsers(RequestManager requestManager, IEnumerable<User> users, IEnumerable<AssignedLicense> licensesToAdd, IEnumerable<Guid> licensesToRemove)
         {
 
-            // modify license on each user
+            // This collection will contain the result of each individual request. Note that batches are handled internally by RequestManager, so you only deal with individual requests that were created here.
+            // Operations can fail (e.g. you cannot remove a license the user does not have) - the OperationResult type let's you check if the operation failed and inspect the error response returned by the service.
             IEnumerable<OperationResult<User>> results;
+
+            // This builder allows us to create single operations (i.e. ones that don't require collection handling).
             using (var builder = GraphRequestBuilder<User>.GetBuilderForSingleOperation(requestManager, out results))
             {
                 foreach (var user in users)
                 {
+                    // Standard Graph SDK syntax. Again, the request is not actually executed here.
+                    // ReturnNoContent() is a helper method that adds a header instructing Graph not to return the full user object after it is modified. This allows us to reduce the size 
+                    // of the response to further speed things up.
                     builder.Users[user.Id].AssignLicense(licensesToAdd, licensesToRemove).Request().ReturnNoContent().PostAsync().Wait();
                 }
             }
